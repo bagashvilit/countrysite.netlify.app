c5d3a3fe9f2cb946117d6115276ecb98179ea95d
bagashvilit <bagashvilit@allegheny.edu>
2020-12-06 13:22:53 +0400

    Add first quiz question

--- src/templates/quiz.js
+++ src/templates/quiz.js
@@ -23,12 +23,12 @@ export const pageQuery = graphql`
   }
 `
 
-const Contact = ({data}) => {
+const Quiz = ({data}) => {
   const { markdownRemark, site } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
 
   return  (
-    <Layout className="contact-page">
+    <Layout className="quiz">
       <SEO 
         title={frontmatter.title}
         description={frontmatter.title + " " + site.siteMetadata.title}
@@ -39,7 +39,14 @@ const Contact = ({data}) => {
         <form className="contact-form" action="/thanks" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
           <input type="hidden" name="form-name" value="contact" />
           <p>
-            <label>Name<input type="text" name="name" required /></label>   
+          <label>What is your preferred city?<input type="text" name="name" list="cities" required /></label>
+            <datalist id="cities">
+                <option value="Tbilisi"></option>
+                <option value="Tokyo"></option>    
+                <option value="London"></option>
+                <option value="Singapore"></option>
+                <option value="Berlin"></option>
+            </datalist>
           </p>
           <p>
             <label>Email<input type="email" name="email" required /></label>
@@ -60,4 +67,4 @@ const Contact = ({data}) => {
   )
 }
 
-export default Contact
+export default Quiz
