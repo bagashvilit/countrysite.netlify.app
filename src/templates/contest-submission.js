5304dd27e1623a07552f39b908cb81482edccd93
bagashvilit <bagashvilit@allegheny.edu>
2020-12-07 01:25:40 +0400

    Add script for the js file

--- src/templates/contest-submission.js
+++ src/templates/contest-submission.js
@@ -37,7 +37,7 @@ const contest = ({data}) => {
       <div className="wrapper">
         <h1>{frontmatter.title}</h1>
         <div className="description" dangerouslySetInnerHTML={{ __html: html }} />
-
+        <script type="text/javascript" language = "javascript" src="./contest.js"></script>
         <div id="quiz"></div>
         <button id="submit">Get Results</button>
         <div id="results"></div>
