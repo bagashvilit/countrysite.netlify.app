5311ab1ac68ff4fc224a29d2eac1fcfefc153f2f
bagashvilit <bagashvilit@allegheny.edu>
2020-12-07 10:56:49 +0400

    Put javascript code directly in the file

--- src/templates/contest-submission.js
+++ src/templates/contest-submission.js
@@ -26,58 +26,7 @@ export const pageQuery = graphql`
 const contest = ({data}) => {
   const { markdownRemark, site } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
-  function Country(name, iso, capital, population) {
-    "use strict";
-    //  Add the missing source code in the Country constructor
-    this.name = name;
-    this.iso = iso;
-    this.capital = capital;
-    this.population = population;
-  }
-  (function() {
-    "use strict";
-    //  create an array of sample country objects
-    var countries = [
-      new Country("Bahamas", "BS", "Nassau", 301790),
-      new Country("Canada", "CA", "Ottawa", 33679000),
-      new Country("Germany", "DE", "Berlin", 81802257)
-  
-  
-    ];
-    //  you can also push each new country object onto the end of the array
-    countries.push(new Country("Spain", "ES", "Madrid", 46505963));
-    countries.push(new Country("United Kingdom", "GB", "London", 62348447));
-    // Countries I added
-    countries.push(new Country("Georgia", "GE", "Tbilisi", 3989167));
-    countries.push(new Country("Japan", "JP", "Tokyo", 126476461));
-    countries.push(new Country("Thailand", "TH", "Bangkok", 69866092));
-    countries.push(new Country("Philippines", "PH", "Manila",  109581078));
-    countries.push(new Country("Vietnam", "VN", "Hanoi", 97141003));
-  
-    // now loop through all this array of country objects
-    
-    // note that many web developers do not consider it a best practice
-    // to modify the DOM through the document.write method. Please talk
-    // with the course instructor to learn more about this idea.
-    for (var i = 0; i < countries.length; i++) {
-      var c = countries[i];
-      // Add in the source code to start the div marker
-      document.write("<div class= 'box'>");
-      // Convert ISO codes into lowercase because the GatorGrader looks for flag images with lower case names
-      document.write("<img src='img/flags/" + c.iso.toLowerCase() + ".png' class='boxImg'>");
-      document.write("<p>");
-      // now loop through all of the details about the country
-      for (var propertyName in c) {
-        document.write("<strong>");
-        document.write(propertyName + ": ");
-        document.write("</strong>");
-        document.write(c[propertyName]);
-        document.write("<br>");
-      }
-      // Add in the source code to conclude the div marker
-      document.write("</div>")
-    }
-  })();
+
   return  (
     <Layout className="contact-page">
       <SEO 
@@ -89,8 +38,25 @@ const contest = ({data}) => {
         <h1>{frontmatter.title}</h1>
         
         <div className="description" dangerouslySetInnerHTML={{ __html: html }} />
-        <script type="text/javascript" language = "javascript" src="./contest.js"></script>
-      </div>
+      <p>Select a font to load:</p>
+
+
+      <button data-font="Nerko One">Nerko One</button>
+      <button data-font="Raleway">Raleway</button>
+      <button data-font="Ubuntu">Ubuntu</button>
+      <button data-font="Yanone Kaffeesatz">Yanone Kaffeesatz</button>
+
+    
+      <p>Hi! I am Teona Bagashvili, junior at <a href = "https://allegheny.edu/">Allegheny college</a>
+        Majoring in Computer Science and minoring in Dance and Movement Studies.
+        My working experience includes intership at <a href="https://www.napr.gov.ge/">IT department of National Agency of Public Registry</a>(Republic of Georgia),
+        and Technical Leader position at <a href="https://www.cs.allegheny.edu/">Department of Computer Science</a> at Allegheny College.
+        I am Currently working as Allegheny Software Engineer,
+        and <a href="https://csethics.allegheny.edu/">Allegheny Mozilla Fellow</a>.
+      </p>
+    </div>
+    <script type="text/javascript" language = "javascript" src="contest.js"></script>
+     
 
       
     </Layout>
--- src/templates/contest.js
+++ src/templates/contest.js
@@ -1,52 +1,21 @@
-function Country(name, iso, capital, population) {
-  "use strict";
-  //  Add the missing source code in the Country constructor
-  this.name = name;
-  this.iso = iso;
-  this.capital = capital;
-  this.population = population;
-}
-(function() {
-  "use strict";
-  //  create an array of sample country objects
-  var countries = [
-    new Country("Bahamas", "BS", "Nassau", 301790),
-    new Country("Canada", "CA", "Ottawa", 33679000),
-    new Country("Germany", "DE", "Berlin", 81802257)
+var body = document.querySelector('body');
+var fontSelect = document.querySelectorAll('[data-font]');
 
-
-  ];
-  //  you can also push each new country object onto the end of the array
-  countries.push(new Country("Spain", "ES", "Madrid", 46505963));
-  countries.push(new Country("United Kingdom", "GB", "London", 62348447));
-  // Countries I added
-  countries.push(new Country("Georgia", "GE", "Tbilisi", 3989167));
-  countries.push(new Country("Japan", "JP", "Tokyo", 126476461));
-  countries.push(new Country("Thailand", "TH", "Bangkok", 69866092));
-  countries.push(new Country("Philippines", "PH", "Manila",  109581078));
-  countries.push(new Country("Vietnam", "VN", "Hanoi", 97141003));
-
-  // now loop through all this array of country objects
-  
-  // note that many web developers do not consider it a best practice
-  // to modify the DOM through the document.write method. Please talk
-  // with the course instructor to learn more about this idea.
-  for (var i = 0; i < countries.length; i++) {
-    var c = countries[i];
-    // Add in the source code to start the div marker
-    document.write("<div class= 'box'>");
-    // Convert ISO codes into lowercase because the GatorGrader looks for flag images with lower case names
-    document.write("<img src='img/flags/" + c.iso.toLowerCase() + ".png' class='boxImg'>");
-    document.write("<p>");
-    // now loop through all of the details about the country
-    for (var propertyName in c) {
-      document.write("<strong>");
-      document.write(propertyName + ": ");
-      document.write("</strong>");
-      document.write(c[propertyName]);
-      document.write("<br>");
+// Load a Google font by name
+var loadFont = function(font) {
+  WebFont.load({
+    google: {
+      families: [font]
     }
-    // Add in the source code to conclude the div marker
-    document.write("</div>")
-  }
-})();
+  });
+};
+
+// Add an event listener for each button
+// When a button is clicked, get the font name, load the font, and set the new font family
+for (var i = 0; i < fontSelect.length; i++) {
+  fontSelect[i].addEventListener('click', function() {
+    var font = this.getAttribute('data-font');
+    loadFont(font);
+    body.style.fontFamily = font;
+  });
+}
